7d863d4d0d2dc971397338615eca20fc8c036c15
genghuiluo <luogenghui@gmail.com>
2018-01-22 16:00:07 +0800

    add service mysql start

--- otter-manager/start-otter-manager.bash
+++ otter-manager/start-otter-manager.bash
@@ -6,6 +6,8 @@ if [[ -z ${IP+x} ]] || [[ -z ${PORT+x} ]] || [[ -z ${ZK_CLUSTER+x} ]]; then
 	exit 1
 fi
 
+service mysql start
+
 sed -i "s/access_ip_placeholder/$IP/g" /opt/otter-manager/conf/otter.properties
 sed -i "s/access_port_placeholder/$PORT/g" /opt/otter-manager/conf/otter.properties
 sed -i "s/zookeeper_cluster_placeholder/$ZK_CLUSTER/g" /opt/otter-manager/conf/otter.properties
